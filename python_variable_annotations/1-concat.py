16eb69bc227816fb1b690e28baa89fc3db2c0ec7
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 14:12:07 +0200

    pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,7 +2,9 @@
 """
 Task1 : Basic annotations - concat
 """
-def concat(str1 : str, str2 : str) -> str :
+
+
+def concat(str1: str, str2: str) -> str:
     """
     Fonction qui prend 2 chaines de caractères qui les concatène
     et renvoie leur concaténation (str1 + str2)

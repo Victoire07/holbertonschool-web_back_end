3c6a203de4250028a40e6f09c92bf9224cba9d28
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 16:12:03 +0200

    ajout du return permettant d'avoir la somme de la liste des floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,11 +2,12 @@
 """
 Task5: Complex types - list of floats
 """
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Fonction qui prend en argument une liste de float → input_list puis
     retourne la somme de cette liste, qui est un float
     """
-    return (float(input_list))
+    return (sum(input_list))

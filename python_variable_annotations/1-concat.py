2f96293ddda691e29b90f046af3f4d57ef9cf025
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 14:09:07 +0200

    fonction qui concatène 2 strings et renvoie leur concaténation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Task1 : Basic annotations - concat
+"""
+def concat(str1 : str, str2 : str) -> str :
+    """
+    Fonction qui prend 2 chaines de caractères qui les concatène
+    et renvoie leur concaténation (str1 + str2)
+    """
+    return str(str1 + str2)

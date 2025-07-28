af9f0f798ed42c301f6465c6146ea43876783e24
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 16:55:31 +0200

    chmod +x

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Task7: Complex types - string and int/float to tuple
+"""
+from typing import Union
+
+def to_kv(k: str, v: Union[int, float]):
+    """
+    Retourne un tuple contenant une chaîne et le carré d’un nombre,
+    sous forme de float
+    """

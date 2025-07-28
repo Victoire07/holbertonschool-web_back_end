38dd84040c4929e1df63309c1df9f0c72cd03045
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 17:24:32 +0200

    docstring fonction +pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,5 +5,8 @@ Task9: Let's duck type an iterable object
 from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst : Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Retourne une liste de 'tuple' avec les élements et leur longueur
+    """
     return [(i, len(i)) for i in lst]

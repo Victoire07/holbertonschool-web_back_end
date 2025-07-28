facafee7b2c3be6bb2ca1bcea3addeb6f3090635
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 17:17:57 +0200

    respect de la norme pycodestyle t7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ Task7: Complex types - string and int/float to tuple
 """
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Retourne un tuple contenant une chaîne et le carré d’un nombre,

3dd5d194dce0148130cca9e307624d556a062948
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 13:56:13 +0200

    ma version pour cette task mais non fonctionnel lors de l'exécution

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Task0 : Basic annotations - add
+"""
+def add (floata : float, floatb : float):
+    """
+    Fonction qui aditionne 2 floats et retourne la somme
+    """
+    return float(floata + float b)

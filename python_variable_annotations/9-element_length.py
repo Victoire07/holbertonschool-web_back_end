fd0ffbf16363a56cf26037e34c3af4c6ab250c0d
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 17:22:49 +0200

    ajout retour sous forme list de tuple

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,5 +5,5 @@ Task9: Let's duck type an iterable object
 from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst):
+def element_length(lst : Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]

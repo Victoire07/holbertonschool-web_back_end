b004e7c31ff45290e04eadf9428e47563256ac03
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 16:57:23 +0200

    import aussi de Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,10 +2,11 @@
 """
 Task7: Complex types - string and int/float to tuple
 """
-from typing import Union
+from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]):
     """
     Retourne un tuple contenant une chaîne et le carré d’un nombre,
     sous forme de float
     """
+    return (Tuple[str, float])

54eb8ef6d5061088295028c38239ec31347b474f
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 15:40:09 +0200

    ajout fonction qui transforme float en string sans le return encore

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Task3: Basic annotations - to string
+"""
+def to_str(n: float) -> str:
+    """
+    Fonction qui va transformer un float en string
+    """
+    return ()

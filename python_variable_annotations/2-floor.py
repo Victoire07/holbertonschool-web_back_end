c2b05cf097a1a8088a024824b6152e2ec4a55c03
Victoire07 <victoireboutin07@gmail.com>
2025-07-28 15:18:30 +0200

    pycodestyle task2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,8 +3,10 @@
 Task2: Basic annotations - floor
 """
 import math
+
+
 def floor(n: float) -> int:
     """
     Retourne la partie entière inférieure d’un nombre flottant
     """
-    return(math.floor(n))
+    return (math.floor(n))

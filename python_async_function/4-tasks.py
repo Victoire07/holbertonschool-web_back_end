95fc3e0fea090fbb6bdfd9de81270057c7f5f3cb
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 13:42:33 +0200

    génération d'une liste de n tâches avec task_wait_random

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,11 +3,16 @@
 Task 4: Tasks
 """
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
-async def task_wait_n(n, max_delay):
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     """
-    
+    task_list = []
+    for i in range(n):
+        task_list.append(task_wait_random(max_delay))
+
 
 

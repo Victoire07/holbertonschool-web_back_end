5bb3342fcb3d96366a0dcccbc2c78c069f6d2219
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 12:57:03 +0200

    mesure du temps d’exécution total de wait_n et calcul de la durée moyenne par coroutine

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,9 +3,16 @@
 Task2: Measure the runtime
 """
 import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """
+    Mesure le temps total d’exécution de wait_n(n, max_delay)
+    et retourne la durée moyenne d’attente par coroutine.
     """
-
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    duration = end - start
+    return (duration / n)

bf505dcb2ca753f2bd8de9d297d1531aab296623
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 17:39:23 +0200

    fctn qui boucle 10 fois et génére un float aléatoire entre 1 et 10 max

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 0: Async Generator
+"""
+import asyncio
+import random
+
+async def async_generator():
+    """
+    Générateur asynchrone qui boucle 10 fois
+    À chaque itération, attend 1 seconde et génère un float aléatoire
+    entre 0 et 10
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

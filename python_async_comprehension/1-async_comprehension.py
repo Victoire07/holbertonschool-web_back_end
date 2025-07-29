7cbc0b529d90388e98793cbe26ea429cd7f594be
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 18:33:48 +0200

    permission

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,12 @@
-#!/usr/bin:env python3
+#!/usr/bin/env python3
 """
 Task1 : Async Comprehensions
 """
 async_generator = __import__("0-async_generator").async_generator
 
+
+async def async_comprehension():
+    """
+    Coroutine collecte 10 nombres aléatoires générés par un
+    générateur asynchrone et les retourne sous forme de liste
+    """

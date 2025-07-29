4905cd5c40ea0b8f5381c6b32352cf70a15ed93d
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 13:46:20 +0200

    donnation permission + récupération des délais dans l'ordre d'arrivée via asyncio.as_completed

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -13,6 +13,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     task_list = []
     for i in range(n):
         task_list.append(task_wait_random(max_delay))
-
-
-
+    
+    results = []
+    for task in asyncio.as_completed(task_list):
+        result = await task
+        results.append(result)
+    return (results)

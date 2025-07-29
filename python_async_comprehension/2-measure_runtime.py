c84f5fa373a7473d128c9162dc0a2ed017546163
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 20:40:53 +0200

    Ajout de la mesure de runtime pour 4 async_comprehension en parallèle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -11,3 +11,14 @@ async def measure_runtime():
     """
     Mesure le temps total pour exécuter 4 fois async_comprehension en parallèle
     """
+    start = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.perf_counter()
+    total_duration_variable = end - start
+    average_duration = total_duration_variable / 4
+    return average_duration

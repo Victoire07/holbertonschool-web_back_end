cbd58c29258852c59a61a05a838a2fd0dac29e61
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 03:15:17 +0200

    def de la fonction puis créa variable

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 1: Let's execute multiple coroutines at the same time with async
+"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay : int) -> List[float]:
+    """
+    Générer wait_random n fois avec le max_delay spécifié.
+    Renvoie la liste de tous les délais (valeurs flottantes)
+    """
+    all_tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+

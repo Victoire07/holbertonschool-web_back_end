2d88eca79c335703407d566fc4d9912b7c59fb08
Victoire07 <victoireboutin07@gmail.com>
2025-07-29 02:54:50 +0200

    prmmission + implémentation de wait_random (coroutine async), utilisation de asyncio et random.uniform avec typage et docstring

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 0: The basics of async
+"""
+import random, asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Fonction qui attend un délai aléatoire compris
+    entre 0 et max_delay (inclus),
+    puis retourne ce délai en secondes sous forme de float
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return (delay)

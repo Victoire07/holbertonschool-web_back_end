9e55796b21669d9b47c943f39cb24c39c8c855c2
Victoire07 <victoireboutin07@gmail.com>
2025-08-14 01:06:33 +0200

    verification si entier positif et non négatif pour page et page_size

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -38,8 +38,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and 0 > page
-        assert isinstance(page_size, int) and 0 > page_size
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         data = self.dataset()
 

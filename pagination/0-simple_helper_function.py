028e43499cf31616b0132cf0e3cc4fd79d405136
Victoire07 <victoireboutin07@gmail.com>
2025-08-13 23:54:21 +0200

    Pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ Task 0 -> Simple helper function
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of size two containing a start index and an end index.

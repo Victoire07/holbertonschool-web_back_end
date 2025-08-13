8fec3d7ac2a95c35a508b6096f1c5b84632ff92f
Victoire07 <victoireboutin07@gmail.com>
2025-08-14 01:54:13 +0200

    modif pycodestyle

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -68,7 +68,16 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """
-        Docstring a faire après ave chcat
+        Retourne les métadonnées de pagination hypermédia ainsi
+        que le contenu de la page
+
+        Clés :
+        - page_size : nombre réel d’éléments renvoyés pour cette page
+        - page : numéro de la page courante
+        - data : liste des lignes pour la page demandée
+        - next_page : numéro de la page suivante ou None
+        - prev_page : numéro de la page précédente ou None
+        - total_pages : nombre total de pages
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
@@ -82,13 +91,10 @@ class Server:
         next_page = page + 1 if page < total_pages else None
 
         return {
-        "page_size": len(data),  # taille réelle de la page renvoyée
-        "page": page,
-        "data": data,
-        "next_page": next_page,
-        "prev_page": prev_page,
-        "total_pages": total_pages,
+            "page_size": len(data),  # taille réelle de la page renvoyée
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
         }
-
-
-

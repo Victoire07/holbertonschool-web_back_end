0b4ce8574602b444b04a4a45d7c73c3722aba9f4
Victoire07 <victoireboutin07@gmail.com>
2025-08-14 01:37:54 +0200

    definition de la fnctn

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -38,11 +38,20 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and 0 < page
-        assert isinstance(page_size, int) and 0 < page_size
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         data = self.dataset()
 
         start, end = index_range(page, page_size)
+
+        if start >= len(data):
+            return []
+
         return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        """
+
 

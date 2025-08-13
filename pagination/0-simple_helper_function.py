853435bafad925f153b8b0854a3f4f27da6afc50
Victoire07 <victoireboutin07@gmail.com>
2025-08-13 13:21:14 +0200

    création fichier task 0 + ajout shebang et donction + docstring

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Task 0 -> Simple helper function
+"""
+
+def index_range(page, page_size):
+    """
+    Returns a tuple of size two containing a start index and an end index.
+    """

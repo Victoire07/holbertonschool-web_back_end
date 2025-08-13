006bb80566e79b8c50d9a769c28148eb83a46945
Victoire07 <victoireboutin07@gmail.com>
2025-08-14 01:02:06 +0200

    ajt fichier 1 dans la t2

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Task2 -> Hypermedia pagination
+"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and 0 < page
+        assert isinstance(page_size, int) and 0 < page_size
+
+        data = self.dataset()
+
+        start, end = index_range(page, page_size)
+        return data[start:end]
+

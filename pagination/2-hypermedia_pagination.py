431402a0935ed0567a0dbaca79a7333ce309397c
Victoire07 <victoireboutin07@gmail.com>
2025-08-14 01:50:31 +0200

    ajt de la méthode pour renvoyer un dico non fonctionnel encore

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -38,6 +38,22 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Renvoie une page spécifique de l'ensemble de données.
+
+        Arguments :
+        °page (int) : numéro de la page à récupérer (indexée à partir de 1).
+        °0 page_size (int) : nombre d'éléments par page.
+
+        Retour:
+        ° List[List] : une sous-liste de l'ensemble de données correspondant
+        à la page demandée. Retourne une liste vide sil'index de départ est en
+        dehors de la plage de l'ensemble de données.
+
+        Lève :
+        ° AssertionError : si `page` ou `page_size` ne sont pas
+        des entiers positifs.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -52,6 +68,27 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """
+        Docstring a faire après ave chcat
         """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.get_page(page, page_size)
+
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        return {
+        "page_size": len(data),  # taille réelle de la page renvoyée
+        "page": page,
+        "data": data,
+        "next_page": next_page
+        "prev_page": prev_page
+        "total_pages": total_pages,
+        }
+
 
 

995b55548df6313b1bbc65dc2ae870ec044b93b6
Victoire07 <victoireboutin07@gmail.com>
2025-08-13 15:05:26 +0200

    ° Donnation des permissions aux 2 fichiers avec chmod / ° Ajout du caclul start et end / ° ajout du return

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,8 +2,12 @@
 """
 Task 0 -> Simple helper function
 """
+from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of size two containing a start index and an end index.
     """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)

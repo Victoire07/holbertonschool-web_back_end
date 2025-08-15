fb88ab2a88160ba398951ff520b716ae8679132e
Victoire07 <victoireboutin07@gmail.com>
2025-08-15 20:38:17 +0200

    ajt de la docstri,g généré par gpt

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task9: Insert a document in Python
+"""
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insère un nouveau document dans une collection MongoDB.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+            La collection MongoDB dans laquelle insérer le document.
+        **kwargs:
+            Paires clé-valeur représentant les champs et valeurs
+            du document à insérer.
+
+    Returns:
+        ObjectId: L'identifiant unique (_id) du document inséré.
+    """

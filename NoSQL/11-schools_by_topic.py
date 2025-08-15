385228984a9af0e61c98ee4f40bb6aa2eb245e2f
Victoire07 <victoireboutin07@gmail.com>
2025-08-15 20:59:03 +0200

    Task11 retiurne liste ecole avec sujet specifique dans le champs 'topics'

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,5 +4,17 @@ Task11: Where can I learn Python?
 """
 def schools_by_topic(mongo_collection, topic):
     """
+     Retourne la liste des écoles ayant un sujet spécifique dans leur champ 'topics'.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+            La collection MongoDB à interroger.
+        topic (str):
+            Le sujet à rechercher dans la liste 'topics' des écoles.
+
+    Returns:
+        list: Une liste de documents (dictionnaires) correspondant
+              aux écoles qui contiennent le sujet donné.
+              Retourne une liste vide si aucune école ne correspond.
     """
-    pass
+    return list(mongo_collection.find({"topics": topic}))

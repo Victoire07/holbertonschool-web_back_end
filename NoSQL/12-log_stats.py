d14672e1e9d7df3e85c1448a73fa5246d90e23e7
Victoire07 <victoireboutin07@gmail.com>
2025-08-18 16:18:13 +0200

    ajout d'une premiere verison pas du tout fonctionnelle

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,3 +2,27 @@
 """
 Task12: Log stats
 """
+from pymongo import MongoClient
+client = MongoClient()
+db = client.logs
+collection = db.nginx
+
+
+collection.count_documents({})
+print("x logs")
+
+collection.count_documents({"method": "GET"})
+print("GET")
+
+collection.count_documents({"method": "POST"})
+print("POST")
+
+collection.count_documents({"method": "PUT"})
+print("PUT")
+
+collection.count_documents({"method": "DELETE"})
+print("DELETE")
+
+collection.count_documents({"method": "GET", "path": "/status"})
+print("y status check")
+

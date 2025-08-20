8d01ce13b01f977cac12ce4fe5d149bd317304e4
Victoire07 <victoireboutin07@gmail.com>
2025-08-20 20:58:08 +0200

    norme eslint

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,19 +1,20 @@
-import Currency from '3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-     if (typeof amount !== 'number') {
+    if (typeof amount !== 'number') {
       throw new TypeError('amount must be a number');
     }
-    if (typeof currency !== '') {
-      throw new TypeError('')
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
     }
 
+    // Passation des attributs privée
     this._amount = amount;
     this._currency = currency;
   }
 
-// Getter and Setter for amount
+  // Getter and Setter for amount
   get amount() {
     return this._amount;
   }
@@ -25,7 +26,25 @@ export default class Pricing {
     this._amount = newAmount;
   }
 
+  // Getter and Setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  // Method displayFullPrice
   displayFullPrice() {
-    return `${this._amount}`;
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  // Static Method:
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
   }
 }

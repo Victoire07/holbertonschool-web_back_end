793f12e5a2ce9012d72eff4f51776051c4666bf1
Victoire07 <victoireboutin07@gmail.com>
2025-08-20 21:30:27 +0200

    Modif pour respect de la norme

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,18 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') {
-        throw new TypeError ('sqft must be a number')
+      throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
   }
+
   // getter pour sqft :
   get sqft() {
     return this._sqft;
   }
+
   // Methode 'evacuationWarningMessage'
   evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }

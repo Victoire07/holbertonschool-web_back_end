30675909f5d91dbb268d59c4ceefb6f89a5d5863
Victoire07 <victoireboutin07@gmail.com>
2025-08-20 21:40:27 +0200

    ajout de la vérification que les sous-classes de Building redéfinissent evacuationWarningMessage

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,7 +4,13 @@ export default class Building {
       throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
+
+  if (this.constructor !== Building &&
+    this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
   }
+
+}
 
   // getter pour sqft :
   get sqft() {

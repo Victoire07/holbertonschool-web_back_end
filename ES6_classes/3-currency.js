1ab1d39c54587284beffd8164304bc06a767b1ae
Victoire07 <victoireboutin07@gmail.com>
2025-08-20 19:55:41 +0200

    IDK JARRIVE PAS

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,10 +1,10 @@
 export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
+      throw new TypeError('Code must be a string');
     }
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     }
 
     this._code = code;
@@ -33,5 +33,9 @@ export default class Currency {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }

9f261c4c605ef612088f9498fff16afd6ff28f87
Victoire07 <victoireboutin07@gmail.com>
2025-08-20 20:45:03 +0200

    manque juste tout par rapport a currency mauvaise compréhension des attentes

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -2,6 +2,30 @@ import Currency from '3-currency'
 
 export default class Pricing {
   constructor(amount, currency) {
-    
+     if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof currency !== '') {
+      throw new TypeError('')
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+// Getter and Setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  displayFullPrice() {
+    return `${this._amount}`;
   }
 }

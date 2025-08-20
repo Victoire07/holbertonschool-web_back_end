5c6cc5f1adc53e3eef7297ced8a24c6b158f45ee
Victoire07 <victoireboutin07@gmail.com>
2025-08-20 22:08:02 +0200

    ajt methhode et getters

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -7,6 +7,19 @@ export default class SkyHighBuilding extends Building {
       throw new TypeError('floors must be a number')
     }
 
+    this._sqft = sqft;
+    this._floors = floors;
+  }
 
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+
+  evacuationWarningMessage() {
+    return('Evacuate slowly the NUMBER_OF_FLOORS floors')
+  }
   }
 }

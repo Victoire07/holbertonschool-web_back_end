c0df114980035d7cfc3a389091caac7a7dc546c1
Victoire07 <victoireboutin07@gmail.com>
2025-08-22 15:03:42 +0200

    nom maj

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
+      throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
 

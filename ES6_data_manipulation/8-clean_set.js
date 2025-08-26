d143a7584fe9d72397a44bd236997735d157bd12
Victoire07 <victoireboutin07@gmail.com>
2025-08-26 14:41:49 +0200

    ajt si startString vide retourne chaine vide

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,9 @@
 export default function cleanSet(Set, startString) {
+  // 1- Si startString est vide -> retourne une chaîne vide
+  if (startString = " ") {
+    return '';
 
+  // 2- Créer un tableau pour stocker les morceaux valides
+  const result = [];
+  }
 }

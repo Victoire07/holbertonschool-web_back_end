40b3335e22d118787d6352007ce58f6e47e756e2
Victoire07 <victoireboutin07@gmail.com>
2025-08-26 20:30:07 +0200

    ajout code task 9 non fonctionnel  à l'execution

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,9 @@
 export default function groceriesList() {
-    
+  const groceries = new Map()
+    groceriesList.set ('Apples', 10);
+    groceriesList('Tomatoes', 10);
+    groceriesList('Pasta', 1);
+    groceriesList('Rice', 1);
+    groceriesList('Banana', 5);
+    return groceries;
 }

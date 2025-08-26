44b8e7ed4cde3b63e535eebef019881e92dd717c
Victoire07 <victoireboutin07@gmail.com>
2025-08-26 12:10:44 +0200

    ajout nouvel objet étudiant avce champ grade + return

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,11 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
   const studentsInCity = listStudents.filter((student) => student.location === city);
+
+  const studentsWithGrades = studentsInCity.map((student) => {
+    const found = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+    const grade = found ? found.grade : 'N/A';
+    return { ...student, grade };
+  });
+
+  return studentsWithGrades;
 }

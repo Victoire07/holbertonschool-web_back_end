10b69bf508ae26a0af8568d10c6ca7f057c93d2f
Victoire07 <victoireboutin07@gmail.com>
2025-08-26 14:57:26 +0200

    Fix task8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,23 +1,23 @@
 export default function cleanSet(set, startString) {
   // 1- Si startString est vide -> retourne une chaîne vide
-  if (startString === " ") {
+  if (startString.length === 0 || typeof startString !== 'string') {
     return '';
+} 
 
   // 2- Créer un tableau pour stocker les morceaux valides
   const result = [];
-  }
 
   // 3- Parcourir toutes les valeurs du set
   for (const value of set) {
     // Vérifier si la valeur commence bien par startString
-    if (__________) {
+    if (value.startsWith(startString)) {
       // Découper la fin de la valeur après startString
-      const part = __________;
+      const part = value.slice(startString.length);
       // Ajouter au tableau result
       result.push(part);
     }
   }
 
   // 4- Retourner les morceaux assemblés avec "-"
-  return result.__________;
+  return result.join('-');
 }

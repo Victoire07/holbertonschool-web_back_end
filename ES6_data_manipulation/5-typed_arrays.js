c4ba2b01c5f8e7e1916b818a7b325231f7df4af1
Victoire07 <victoireboutin07@gmail.com>
2025-08-26 12:38:18 +0200

    modif pour respect norme, eslint task5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
 
   // Étape n°2 -> Vérification de la position
   if (position < 0 || position >= length) {
-    throw new Error("Position outside range")
+    throw new Error('Position outside range');
   }
 
   // Étape n3 -> Création d'un dataview basé sur ce buffer
@@ -13,7 +13,7 @@ export default function createInt8TypedArray(length, position, value) {
   // Étape 4 -> ecrire la valeur
   dataView.setInt8(position, value);
 
-  // Étape n5 -> Retourner le dataview 
+  // Étape n5 -> Retourner le dataview
 
-    return dataView
+  return dataView;
 }

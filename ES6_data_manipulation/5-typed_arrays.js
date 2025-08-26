48fc78a3cf8e1832fe273acd63f823788e9f864f
Victoire07 <victoireboutin07@gmail.com>
2025-08-26 12:26:15 +0200

    ajout des étapes

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,17 @@
 export default function createInt8TypedArray(length, position, value) {
-  c
+  // Étape 1 -> Créer le buffer
+  const buffer = new ArrayBuffer(length);
+
+  // Étape n°2 -> Vérification de la position
+  if (position >= length) {
+    throw new Error("Position outside range")
+  }
+
+  // Étape n3 -> Création d'un dataview basé sur ce buffer
+  const dataView = new DataView(buffer);
+
+  // Étape 4 -> ecrire la valeur
+  dataView.setInt8(position, value);
+
+    return DataView
 }

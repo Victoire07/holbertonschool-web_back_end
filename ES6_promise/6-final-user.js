46f06e072e255b7acc8e112f45992a34d7e46db3
Victoire07 <victoireboutin07@gmail.com>
2025-08-28 13:45:53 +0200

    création de 2 const pour stocker les 2 promesses

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-
+  const userPromise = signUpUser(firstName, lastName)
+  const photoPromise = uploadPhoto(filename)
 }

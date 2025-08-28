dd6a683bba7d4d9cbcb1408be2e9f5012a8d5308
Victoire07 <victoireboutin07@gmail.com>
2025-08-28 14:23:47 +0200

    modif code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(filename);
-  return Promise.allSettled([userPromise, photoPromise])
-  .then((results) => {
-    return results.map((r) => {
-        if (r.status === 'fulfilled') {
-          return { status: r.status, value: r.value };
-        }
-        return { status: r.status, value: r.reason };
-      });
-    });
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => (
+      result.status === 'fulfilled'
+        ? { status: result.status, value: result.value }
+        : { status: result.status, value: result.reason }
+    )));
 }

be25eff1179959991c3a8180b03e0f24f46a9198
Victoire07 <victoireboutin07@gmail.com>
2025-08-28 13:53:27 +0200

    Ajout de la fonction handleProfileSignup (Task 6) avec gestion de plusieurs promesses via Promise.allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const userPromise = signUpUser(firstName, lastName)
-  const photoPromise = uploadPhoto(filename)
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(filename);
+  return Promise.allSettled([userPromise, photoPromise])
+  .then((results) => {
+    return results.map((r) => {
+        if (r.status === 'fulfilled') {
+          return { status: r.status, value: r.value };
+        }
+        return { status: r.status, value: r.reason };
+      });
+    });
 }

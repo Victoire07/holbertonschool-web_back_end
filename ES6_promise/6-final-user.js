f5815379ca2cad05c1fd55f80332172769c0bacb
Victoire07 <victoireboutin07@gmail.com>
2025-08-28 15:15:47 +0200

    fix t6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,10 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises)
-    .then((results) => results.map((result) => (
-      result.status === 'fulfilled'
-        ? { status: result.status, value: result.value }
-        : { status: result.status, value: result.reason }
-    )));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ])
+  .then((results) => results);
 }

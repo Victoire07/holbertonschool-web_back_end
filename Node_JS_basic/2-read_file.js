28c9f87e97861b8d99b433da299e206135489ec3
Victoire07 <victoireboutin07@gmail.com>
2025-09-03 23:30:06 +0200

    ajt du try catch et leve erreyr

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,4 +1,11 @@
+// importer fs (jso faire encore regarder google après)
 function countStudents(path) {
+  try {
+    (process.stdin.setEncoding('utf8'));
+  }
+  catch {
+    throw new ('Cannot load the database')
+  }
     
 }
 module.exports = countStudents;

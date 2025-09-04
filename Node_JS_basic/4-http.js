438913d7319d884bc06dac862950bbf1597a44f1
Victoire07 <victoireboutin07@gmail.com>
2025-09-04 14:39:57 +0200

    ajout de req et res

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,5 +1,18 @@
+// importe le module http
 http=require("http");
 
+// créer serveur avec fonction callback
 app=http.createServer(callback);
 
-app.listen(1245)
+app.on("request", (req, res) => {
+  res.write("Hello Holberton School")
+
+})
+
+// lance le serveur sur le port ici 1245
+app.listen(1245, () => {
+  console.log("Hello Holberton School")
+
+})
+
+module.exports = app;

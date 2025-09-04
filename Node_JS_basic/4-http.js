35a384f6255c056bbe254441fbe296f6427f210b
Victoire07 <victoireboutin07@gmail.com>
2025-09-04 14:45:42 +0200

    modif code ajt res.end pour terminer réponse

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,18 +1,17 @@
 // importe le module http
-http=require("http");
+const http=require("http");
 
 // créer serveur avec fonction callback
-app=http.createServer(callback);
+const app=http.createServer(callback);
 
 app.on("request", (req, res) => {
-  res.write("Hello Holberton School")
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School');
 
-})
+});
 
 // lance le serveur sur le port ici 1245
-app.listen(1245, () => {
-  console.log("Hello Holberton School")
+app.listen(1245)
 
-})
 
 module.exports = app;

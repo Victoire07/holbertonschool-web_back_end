9a587588947fe5016d463dc2975a6d48e044bbb4
Victoire07 <victoireboutin07@gmail.com>
2025-09-04 14:53:04 +0200

    norme eslint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,15 +1,13 @@
 // importe le module http
-const http=require("http");
+const http = require('http');
 
 // créer serveur avec fonction callback
-const app=http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School');
-
 });
 
 // lance le serveur sur le port ici 1245
-app.listen(1245)
-
+app.listen(1245);
 
 module.exports = app;

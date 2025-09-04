be4e3cc1260a0e571953f0745188c8382d6d327b
Victoire07 <victoireboutin07@gmail.com>
2025-09-04 10:35:15 +0200

    ajt de la condition avec l'erreur

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,16 @@
+const { rejects } = require('assert');
 const fs = require('fs');
+const { resolve } = require('path');
 function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
+    });
+  });
 }
 
 module.exports = countStudents;

f4e5c3733fe1fbdb81020fccfd5f4ca8c956e139
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 11:32:55 +0200

    norme eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Quand une ligne arrive, afficher "Your name is: ..."
 process.stdin.on('data', (data) => {
-    const name = data;
-    process.stdout.write(`Your name is: ${name}`);
-    process.exit(0);
+  const name = data;
+  process.stdout.write(`Your name is: ${name}`);
+  process.exit(0);
 });
 
 process.on('exit', () => {

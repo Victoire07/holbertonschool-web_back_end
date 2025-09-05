b1b66dcba166d336e578b39d16f24f4d389861a7
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 11:32:21 +0200

    supression saut de ligne

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Quand une ligne arrive, afficher "Your name is: ..."
 process.stdin.on('data', (data) => {
     const name = data;
-    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write(`Your name is: ${name}`);
     process.exit(0);
 });
 

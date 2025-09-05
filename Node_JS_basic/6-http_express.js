8dd9af0d6a0f265dd26a2f8ce32d990f1e4b1882
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 10:29:21 +0200

    ajout du serveur HTTP pour l'exercice 6 (port 1245, route /)

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+// création d'1 instance d’application
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;

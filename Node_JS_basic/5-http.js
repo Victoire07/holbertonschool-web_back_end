332de25cae64f1ef44358b5e6bf78ad638236dd9
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 09:33:59 +0200

    import fnctn t3, port 1245, créa serveur

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,1 +1,8 @@
+const countStudents = require('./3-read_file_async');
 const http = require('http');
+
+const app = http.createServer((req, res) => {
+});
+
+// lance le serveur sur le port ici 1245
+app.listen(1245);

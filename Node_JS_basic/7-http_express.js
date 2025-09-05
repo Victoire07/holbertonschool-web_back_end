589582b97ca4e3783769d74a7815c245b33ddd40
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 15:31:25 +0200

    ajt des 2 routes

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,3 +2,25 @@ const express = require('express');
 
 // création d'1 instance d’application
 const app = express();
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.type('text/plain');
+  res.write('This is the list of our students\n');
+
+  const dbPath = process.argv[2];
+  if (!dbPath) {
+    res.end('Cannot load the database');
+    return;
+  }
+
+  // (on mettra la lecture + formatage ici à l’étape 3)
+});
+
+app.listen(1245);
+
+module.exports = app;

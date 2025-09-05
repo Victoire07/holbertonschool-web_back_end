aa2f7a47d9a92b550a971effc43704f7a3bc5269
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 11:29:07 +0200

    modif code pour réponde aux attentes du checker

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,31 +1,13 @@
 // Afficher la question d’accueil :
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Préparer l’"écoute" sur l’entrée standard
-process.stdin.setEncoding('utf8');
-
-// Pour afficher le nom que 1 seule fois
-let printed = false;
-
 // Quand une ligne arrive, afficher "Your name is: ..."
-process.stdin.on('data', (chunk) => {
-  if (!printed) {
-    const name = chunk.trim();
+process.stdin.on('data', (data) => {
+    const name = data;
     process.stdout.write(`Your name is: ${name}\n`);
-    printed = true;
-  }
+    process.exit(0);
 });
 
-let closed = false;
-function closeOnce() {
-  if (closed) return;
-  closed = true;
-  process.stdout.write('This important software is now closing\n');
-}
-
-process.stdin.on('end', closeOnce);
-
-process.on('SIGINT', () => {
-  closeOnce();
-  process.exit(0);
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });

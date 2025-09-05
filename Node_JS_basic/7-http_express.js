33ca8a58beca1c96f74d9406b30657f8479c4594
Victoire07 <victoireboutin07@gmail.com>
2025-09-05 15:40:26 +0200

    fix task7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,26 +1,59 @@
 const express = require('express');
+const fs = require('fs').promises;
+    
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-// création d'1 instance d’application
+      const lines = data.split('\n');
+      const cleanLines = lines.filter((line) => line.trim() !== '');
+      const rows = cleanLines.slice(1);
+      const students = rows.map((line) => line.split(','));
+
+      const totalStudents = students.length;
+      let output = `Number of students: ${totalStudents}\n`;
+
+      const fields = {};
+      for (const student of students) {
+        const firstname = student[0];
+        const field = student[3];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      for (const fieldName of Object.keys(fields)) {
+        const names = fields[fieldName].join(', ');
+        output += `Number of students in ${fieldName}: ${fields[fieldName].length}. List: ${names}\n`;
+      }
+      resolve(output.trim());
+    });
+  });
+}
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.type('text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.type('text/plain');
-  res.write('This is the list of our students\n');
-
-  const dbPath = process.argv[2];
-  if (!dbPath) {
-    res.end('Cannot load the database');
-    return;
+app.get('/students', async (req, res) => {
+  try {
+    const dataStudents = await countStudents(process.argv[2]);
+    res.setHeader('Content-type', 'text/plain');
+    res.status(200).send(`This is the list of our students\n${dataStudents}`);
+  } catch (err) {
+    res.send(err.message);
   }
-
-  // (on mettra la lecture + formatage ici à l’étape 3)
 });
 
-app.listen(1245);
+if (require.main === module) {
+ app.listen(1245);
+}
 
 module.exports = app;
